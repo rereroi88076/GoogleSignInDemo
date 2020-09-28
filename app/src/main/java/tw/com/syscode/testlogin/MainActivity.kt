8fb9194fe95c1a9a7a995cb3314b592fd7b89476
rereroi88076 <rereroi88076@gmail.com>
2020-09-28 14:16:31 +0800

    20200928 完整版(1)

--- app/src/main/java/tw/com/syscode/testlogin/MainActivity.kt
+++ app/src/main/java/tw/com/syscode/testlogin/MainActivity.kt
@@ -11,10 +11,11 @@ import com.facebook.FacebookCallback
 import com.facebook.FacebookException
 import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
-import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -24,7 +25,7 @@ class MainActivity : AppCompatActivity() {
     val callbackManager = CallbackManager.Factory.create()
 
 
-    private val RC_SIGN_IN: Int = 1
+    private val RC_SIGN_IN: Int = 2
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,33 +57,65 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        val googleSignInOptions = GoogleSignInOptions.Builder(
+        val gso = GoogleSignInOptions.Builder(
             GoogleSignInOptions.DEFAULT_SIGN_IN
         )
-            .requestIdToken(toString())
+            .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .requestId()
             .requestProfile()
             .build()
 
-        val googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
-
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        updateUI(account)
 
         bt_google_login.setOnClickListener {
-                val googleSignInIntent = Auth.GoogleSignInApi.getSignInIntent(googleSignInClient)
-            startActivityForResult(googleSignInIntent, 200)
+            val signInIntent = googleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
         }
-
-
     }
 
-    private fun signIn() {
-        val signInIntent = googleSignInClient.signInIntent
-        startActivityForResult(signInIntent, RC_SIGN_IN)
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account: GoogleSignInAccount = completedTask.getResult(ApiException::class.java)!!
+            Log.d("message", "${account.email}")
+            Log.d("message", "${account.account}")
+            Log.d("message", "${account.id}")
+            Log.d("message", "${account.givenName}")
+            Log.d("message", "${account.idToken}")
+            // Signed in successfully, show authenticated UI.
+            updateUI(account)
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(
+                "123",
+                "signInResult:failed code=" + e.statusCode
+            )
+            updateUI(null)
+        }
     }
+
+    fun updateUI(account: GoogleSignInAccount?) {
+        if (account != null) {
+            Toast.makeText(this, "登入成功", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "登入失敗", Toast.LENGTH_LONG).show()
+        }
+    }
+
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         callbackManager?.onActivityResult(requestCode, resultCode, data)
         super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
     }
+
 }
